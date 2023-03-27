425e6767174f9d007b10a987237244c6aadf5507
idham <idham290502@gmail.com>
2023-03-27 15:39:53 +0800

    idham

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let data1 = true
+let data2 = "1.25"
+console.log(data1, typeof data1)
+console.log(data2, typeof data2)
+
+let data3 = data1.toString
+console.log(data3, typeof data3)
+
+//array
+//let data = ['one']
+
+
+//object
+let user = {
+    name : "idham",
+    faculty : "fkekk",
+    phone : {
+        mobile: "011139611336",
+        home: "7434242347"
+    },
+    email : "idham@gmail.com"
+}
+console.log(user.phone.mobile)
